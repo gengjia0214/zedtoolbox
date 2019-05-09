0d90669505910da314bc208d587c960737d966c4
Jia Geng <gjia0214@hotmail.com>
2019-05-08 20:25:37 -0400

    Initialized virtual environment

--- code/grab_image.py
+++ code/grab_image.py
@@ -1,6 +1,14 @@
 import pyzed.sl as sl
 import numpy as np
+import cv2 as cv
 
 
+filepath = '/media/jgeng/hd1/CobiaSwimmingData/UMEH_111718/Cobia1-Yellow/Yellow.svo';
+init = sl.InitParameters(svo_input_filename=filepath, svo_real_time_mode=False)
+cam = sl.Camera()
+status = cam.open(init)
+if status != sl.ERROR_CODE.SUCCESS:
+    print("fail")
+    exit()
 
 
