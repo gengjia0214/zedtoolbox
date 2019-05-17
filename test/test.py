aa0787e9982d278d4f2b2b4b12865ce257427990
Jia Geng <37158903+gengjia0214@users.noreply.github.com>
2019-05-17 18:09:48 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -17,10 +17,10 @@ def test_manual_sampling_1():
     Test manual_sampling() on output image
     :return: void
     """
-    path = '/media/jgeng/hd1/CobiaSwimmingData/UMEH_111718/Cobia1-Yellow/Yellow.svo'
+    path = 'path/to/file'
     s1 = Sampler(path)
     f_name = "yellow"
-    path = "/home/jgeng/Desktop/"
+    path = 'path/to/output'
     s1.manual_sampling(f_name, path, depth=True)
 
 
@@ -29,10 +29,10 @@ def test_manual_sampling_2():
     Test manual_sampling() on output np array
     :return: void
     """
-    path = '/media/jgeng/hd1/CobiaSwimmingData/UMEH_111718/Cobia1-Yellow/Yellow.svo'
+    path = 'path/to/file'
     s1 = Sampler(path)
     f_name = "yellow"
-    path = "/home/jgeng/Desktop/"
+    path = 'path/to/output'
     left, right, depth = s1.manual_sampling(f_name, path, depth=True, output_format='numpy')
     # image size
     print(left[0].shape)
